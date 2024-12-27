e44050f5b5f0442a78b17aab92cfca4228fc0d6f
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2024-12-27 00:14:41 -0700

    Map update

--- frontend/src/pages/Recap.tsx
+++ frontend/src/pages/Recap.tsx
@@ -171,7 +171,7 @@ const Recap: React.FC = () => {
         </div>
         <div className="flex max-md:flex-col md:gap-4 max-md:gap-2 justify-center items-center md:w-4/6 max-md:w-full">
           <div className="grid grid-cols-1 md:gap-4 max-md:gap-2 max-w-4xl max-md:w-full md:w-full text-center">
-            <div className="stats shadow rounded-md">
+            <div className="stats shadow-lg rounded-md border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">
                   Most played class
@@ -185,7 +185,7 @@ const Recap: React.FC = () => {
               </div>
             </div>
 
-            <div className="stats shadow rounded-md ">
+            <div className="stats shadow-lg rounded-md border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">Matches Played</div>
                 <div className="stat-value max-md:text-base">
@@ -197,7 +197,7 @@ const Recap: React.FC = () => {
               </div>
             </div>
 
-            <div className="stats shadow rounded-md">
+            <div className="stats shadow-lg rounded-md border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">Matches Won</div>
                 <div className="stat-value max-md:text-base">
@@ -219,7 +219,7 @@ const Recap: React.FC = () => {
             </div>
           </div>
           <div className=" text-center max-lg:hidden">
-            <div className="w-64 h-max rounded-lg flex items-center justify-center mb-2">
+            <div className="w-64 h-max rounded-lg flex items-center justify-center mb-2 ">
               <img
                 src={`https://avatars.fastly.steamstatic.com/${
                   id64 && profileData?.steamInfo[id64]?.avatar
@@ -232,8 +232,8 @@ const Recap: React.FC = () => {
               {id64 && profileData?.steamInfo[id64]?.name}
             </h1>
           </div>
-          <div className="grid grid-cols-1 md:gap-4 max-md:gap-2 max-w-4xl max-md:w-full md:w-full text-center">
-            <div className="stats shadow rounded-md">
+          <div className="grid grid-cols-1 md:gap-4 max-md:gap-2 max-w-4xl max-md:w-full md:w-full text-center ">
+            <div className="stats shadow-lg rounded-md border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">KDA</div>
                 <div className="stat-value max-md:text-base">
@@ -249,7 +249,7 @@ const Recap: React.FC = () => {
               </div>
             </div>
 
-            <div className="stats shadow rounded-md">
+            <div className="stats rounded-md shadow-lg border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">
                   {profileData?.topFiveClasses[0].class_name === "medic"
@@ -267,7 +267,7 @@ const Recap: React.FC = () => {
               </div>
             </div>
 
-            <div className="stats shadow rounded-md">
+            <div className="stats shadow-lg rounded-md border border-neutral ">
               <div className="stat max-md:p-3">
                 <div className="stat-title max-md:text-xs">Minutes Played</div>
                 <div className="stat-value max-md:text-base">
@@ -366,7 +366,7 @@ const Recap: React.FC = () => {
               {[1, 2, 3, 4].map((section, index) => (
                 <div key={index} className="flex items-center w-full 2xl:w-1/2">
                   <div className="text-xl font-bold mr-5 max-md:hidden">{section + 1}.</div>
-                  <div className="w-full h-24 bg-base-200 rounded-lg shadow-lg flex items-center mb-4 p-2 relative">
+                  <div className="w-full h-24 bg-base-200 rounded-lg shadow-lg flex items-center mb-4 p-2  border border-neutral  relative">
                     <div className="absolute right-2 top-2 text-xs md:hidden">{section + 1}</div>
                     <img src={`/portraits/${profileData?.topFiveClasses[section].class_name}.png`} className="w-24 h-20 object-cover object-top rounded-l" alt={`${profileData?.topFiveClasses[section]?.class_name} image`}/>
                     <div className="m-3 h-full text-left justify-center items-center w-1/5">
@@ -431,87 +431,62 @@ const Recap: React.FC = () => {
       </div>
 
       {/* Most Played Maps */}
-      <div className="h-screen w-full snap-start flex items-center justify-center bg-base-200">
-        <div className="text-center w-full">
-          <h2 className="text-2xl font-bold mb-4">Your Most Played Maps</h2>
-          <section className="w-full">
-            <div className="flex flex-col items-center w-full md:px-4 max-md:px-2">
-              <div className="flex items-center justify-center w-full 2xl:w-1/2" >
-                <div className=" text-xl font-bold md:mr-5 max-md:hidden">1.</div>
-                <div className="w-full md:h-48 max-md:h-36 bg-base-100 rounded-lg border border-neutral shadow-lg flex items-center justify-center mb-4 relative">
-                <div className="absolute right-2 top-2 text-xs md:hidden">1</div>
-                  <div className="w-full h-full bg-base-100 rounded-lg shadow-lg flex items-center p-2">
-                    
-                    <img src={`/portraits/${profileData?.topFiveMaps[0].map_name}.png`} className="h-full w-[72px] object-cover object-top rounded-l" alt={`${profileData?.topFiveMaps[0].map_name} image`}
-                    />
-                    <div className="m-3 h-full text-left justify-center items-center w-2/5">
-                      <h2 className="md:text-3xl max-md:text-lg font-semibold">
-                        {profileData?.topFiveMaps[0].map_name.charAt(0).toUpperCase() + profileData?.topFiveMaps[0].map_name.slice(1).toLowerCase()}
-                      </h2>
-                      <p className="text-sm">
-                        {(Number(profileData?.topFiveMaps[0].time_played) /60 /60).toFixed(1)}{" "}hrs
-                      </p>
-                      <p className="text-sm">
-                        {profileData?.topFiveMaps[0].matches_played} matches
-                      </p>
-                      <p className="text-sm">
-                        {((profileData?.topFiveMaps[0].wins / profileData?.topFiveMaps[0].matches_played) * 100).toFixed(0)}% winrate
-                      </p>
-                    </div>
-                    <div className="flex justify-center items-center w-full gap-2">
-                      <div className="justify-center items-center">
-                        <div className="md:text-xl max-md:text-lg md:w-full max-md:mr-2 font-bold flex">
-                          <div className="w-1/2 md:mr-10 md:ml-1 max-md:mr-5">WINS</div>
-                          <div className="w-1/2 flex items-baseline text-right md:ml-10 max-md:ml-5">LOSSES</div>
-                        </div>
-                        <div className="md:text-4xl max-md:text-lg font-bold flex w-full">
-                          <div className="w-1/2 text-left md:ml-2.5 max-md:ml-3">{profileData?.topFiveMaps[0]?.wins}</div>
-                          <div className="mx-1">/</div>
-                          <div className="w-1/2 text-right md:mr-3 max-md:mr-7">{profileData?.topFiveMaps[0]?.losses}</div>
-                        </div>
+      <div className="h-screen w-full snap-start flex flex-col items-center justify-center bg-base-200">
+        <div className="flex justify-center items-center w-full 2xl:w-1/2">
+          <div className="text-center text-xl font-bold">MOST PLAYED MAPS</div>
+        </div>
+        {[0, 1, 2, 3, 4].map((section, index) => (
+          <div key={index} className="flex items-center w-full 2xl:w-1/2">
+            <div className="w-full rounded-lg flex items-center m-2 relative">
+              <div className="flex w-full h-24 bg-base-100 rounded-lg shadow-lg border border-neutral max-md:p-2">
+                <div className="absolute right-2 top-2 text-xs md:hidden">{section + 1}</div>
+                <img src={`/portraits/${profileData?.topFiveMaps[section].map_name}.png`} className="md:w-24 md:m-2 md:h-20 max-md:h-22 max-md:w-12 object-cover object-top rounded-l" alt={`${profileData?.topFiveMaps[section].map_name} image`}/>
+                <div className="mx-2 p-1 h-full text-left justify-center items-center md:w-1/5 max-md:w-2/5 border-r border-neutral">
+                  <h2 className="md:text-2xl max-md:text-xs font-bold md:max-w-28 max-md:max-w-20 truncate overflow-hidden">
+                    {profileData?.topFiveMaps[section].map_name.charAt(0).toUpperCase() + profileData?.topFiveMaps[section].map_name.slice(1).toLowerCase()}
+                  </h2>
+                  <p className="md:text-sm max-md:text-xs">
+                  {profileData?.topFiveMaps[section].matches_played} matches
+                  </p>
+                  <p className="md:text-sm max-md:text-xs">
+                  {((profileData?.topFiveMaps[section].wins / profileData?.topFiveMaps[section].matches_played) * 100).toFixed(0)}% winrate                  </p>
+                </div>
+                <div className="w-full pr-3 h-full">
+                <div className="flex justify-center items-center">
+                    <div className="grid grid-cols-2 text-center md:text-lg max-md:text-sm font-bold items-center w-full h-full gap-4 mt-3 mb-1">
+                      <div>
+                        <div className="">WINS</div>
+                        <div className="">{profileData?.topFiveMaps[section]?.wins}</div>
                       </div>
-                    </div>                    
+                      <div>
+                        <div className="">LOSSES</div>
+                        <div className="">{profileData?.topFiveMaps[section]?.losses}</div>
+                      </div>  
+                    </div>
+                  </div>
+                  <div className="h-2 w-full rounded-lg relative mb-1">
+                  {/* Green Success Bar */}
+                    <div
+                      className="bg-success h-2 absolute rounded-l-lg bg-opacity-60 border-r-2 border-base-300"
+                      style={{
+                        width: `${((profileData?.topFiveMaps[section]?.wins / profileData?.topFiveMaps[section]?.matches_played) * 100) || 0}%`,
+                      }}
+                    ></div>
+
+                    {/* Red Error Bar */}
+                    <div
+                      className="bg-error h-2 absolute rounded-r-lg bg-opacity-60"
+                      style={{
+                        left: `${((profileData?.topFiveMaps[section]?.wins / profileData?.topFiveMaps[section]?.matches_played) * 100) || 0}%`,
+                        width: `${100 - ((profileData?.topFiveMaps[section]?.wins / profileData?.topFiveMaps[section]?.matches_played) * 100) || 0}%`,
+                      }}
+                    ></div>
                   </div>
                 </div>
               </div>
-
-              {/* Other Sections */}
-              {[1, 2, 3, 4].map((section, index) => (
-                <div key={index} className="flex items-center w-full 2xl:w-1/2">
-                  <div className="text-xl font-bold mr-5 max-md:hidden">{section + 1}.</div>
-                  <div className="w-full h-24 bg-base-100 rounded-lg shadow-lg flex items-center mb-4 p-2 relative">
-                    <div className="absolute right-2 top-2 text-xs md:hidden">{section + 1}</div>
-                    <img src={`/portraits/${profileData?.topFiveMaps[section].map_name}.png`} className="w-24 h-20 object-cover object-top rounded-l" alt={`${profileData?.topFiveMaps[section].map_name} image`}/>
-                    <div className="m-3 h-full text-left justify-center items-center w-2/5">
-                      <h2 className="md:text-3xl max-md:text-lg font-semibold">
-                        {profileData?.topFiveMaps[section].map_name.charAt(0).toUpperCase() + profileData?.topFiveMaps[section].map_name.slice(1).toLowerCase()}
-                      </h2>
-                      <p className="text-sm">
-                        {profileData?.topFiveMaps[section].matches_played} matches
-                      </p>
-                      <p className="text-sm">
-                        {((profileData?.topFiveMaps[section].wins / profileData?.topFiveMaps[section].matches_played) * 100).toFixed(0)}% winrate
-                      </p>
-                    </div>
-                    <div className="flex justify-center items-center w-full gap-2">
-                      <div className="justify-center items-center">
-                        <div className="md:text-xl max-md:text-lg md:w-full max-md:mr-2 font-bold flex">
-                          <div className="w-1/2 md:mr-10 md:ml-1 max-md:mr-5">WINS</div>
-                          <div className="w-1/2 flex items-baseline text-right md:ml-10 max-md:ml-5">LOSSES</div>
-                        </div>
-                        <div className="md:text-4xl max-md:text-lg font-bold flex w-full">
-                          <div className="w-1/2 text-left md:ml-2.5 max-md:ml-3">{profileData?.topFiveMaps[section]?.wins}</div>
-                          <div className="mx-1">/</div>
-                          <div className="w-1/2 text-right md:mr-3 max-md:mr-7">{profileData?.topFiveMaps[section]?.losses}</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
             </div>
-          </section>
-        </div>
+          </div>
+        ))}
       </div>
 
       {/* Teammates */}
@@ -635,7 +610,7 @@ const Recap: React.FC = () => {
       </div>
 
       {/* Best Teammates & Enemies */}
-      <div className="h-screen w-full snap-start flex flex-col items-center justify-center bg-base-200">
+      <div className="h-screen w-full snap-start flex flex-col items-center justify-center bg-base-300">
         <div className="w-full h-full flex max-md:flex-col justify-center items-center 2xl:w-2/3">
           <div className="w-full">
           <div className="flex justify-center items-center w-full">
@@ -644,7 +619,7 @@ const Recap: React.FC = () => {
           {[0, 1, 2].map((section, index) => (
           <div key={index} className="flex items-center w-full">
             <div className="w-full rounded-lg flex items-center m-2 relative">
-              <div className="flex w-full h-24 bg-base-100 rounded-lg shadow-lg border border-success max-md:p-2">
+              <div className="flex w-full h-24 bg-base-200 rounded-lg shadow-lg border border-success max-md:p-2">
                 <div className="absolute right-2 top-2 text-xs md:hidden">{section + 1}</div>
                 <img src={`https://avatars.fastly.steamstatic.com/${profileData?.winningTeammates[section].teammate_id64 && profileData?.steamInfo[profileData?.winningTeammates[section].teammate_id64]?.avatar}_full.jpg`} className="md:w-24 md:m-2 md:h-20 max-md:h-22 max-md:w-12 object-cover object-top rounded-l" alt={`${profileData?.steamInfo[profileData?.winningTeammates[section].teammate_id64]?.name} image`}/>
                 <div className="mx-2 p-1 h-full text-left justify-center items-center md:w-2/6 max-md:w-2/5 border-r border-neutral">
@@ -756,7 +731,7 @@ const Recap: React.FC = () => {
         </div>
       </div>
 
-      {/* Best Teammates & Enemies */}
+      {/* Worst Teammates & Enemies */}
       <div className="h-screen w-full snap-start flex flex-col items-center justify-center bg-base-200">
         <div className="w-full h-full flex max-md:flex-col justify-center items-center 2xl:w-2/3">
           <div className="w-full">
